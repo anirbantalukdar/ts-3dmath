1da751198538124dbd7fb59e3a0d50b5ab9c9f9e
Anirban Talukdar <talukdar.anirban@gmail.com>
2024-11-06 20:37:49 +0530

    deleted unwanted lib folder

--- lib/ge/Bound2.d.ts
+++
@@ -1,9 +0,0 @@
-import { Point2 } from './Point2';
-export declare class Bound2 {
-    private m_Min;
-    private m_Max;
-    constructor();
-    addPoint(pt: Point2): Bound2;
-    getMin(): Point2;
-    getMax(): Point2;
-}
--- lib/ge/Context.d.ts
+++
@@ -1,7 +0,0 @@
-import { Tolerance } from './Tolerance';
-export declare class Context {
-    private constructor();
-    private static s_Instance;
-    readonly tolerance: Tolerance;
-    static getInstance(): Context;
-}
